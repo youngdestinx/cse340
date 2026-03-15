d443d81c38d024c9eb6dee4ceaae90da41a8b1cf
Destiny Linus <youngdestinx@gmail.com>
2026-03-15 02:11:54 +0100

    site made look profesional

--- server.js
+++ server.js
@@ -47,6 +47,11 @@ app.get('/projects', async (req, res) => {
     res.render('projects', {title})
 });
 
+app.get('/categories', async (req, res) => {
+    const title = 'Services Projects Categories'
+    res.render('categories', {title})
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
     console.log(`Environment: ${NODE_ENV}`);
