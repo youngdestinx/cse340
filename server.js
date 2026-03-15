29975515a543a618837c3256466ba9e6175a0b19
Destiny Linus <youngdestinx@gmail.com>
2026-03-15 01:10:47 +0100

    server site created

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -6,13 +9,45 @@ const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __diirname = path.dirname(__filename);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express!');
+/**
+ * Configure Express Middleware
+ */
+
+//Serve static files from the public directory
+app.use(express.static(path.join(__diirname, 'public')));
+
+
+//Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__diirname, 'src/views'));
+
+/**
+ * Routes
+ */
+
+app.get('/', async (req, res) => {
+    const title = 'Home'
+    res.render('home', {title})
+});
+
+app.get('/organizations', async (req, res) => {
+    const title = 'Our Partner Organizations'
+    res.render('organizations', {title})
+});
+
+app.get('/projects', async (req, res) => {
+    const title = 'Services Projects'
+    res.render('projects', {title})
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://127.0.0.1:${PORT}`);
     console.log(`Environment: ${NODE_ENV}`);
-})
+});
