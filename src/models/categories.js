4d50c7916e09344fcb404491cce4246e1fa40794
Destiny Linus <youngdestinx@gmail.com>
2026-03-26 11:37:50 +0100

    categories fix

--- src/models/categories.js
+++ src/models/categories.js
@@ -2,11 +2,11 @@ import db from './db.js'
 
 const getAllProjectsCategories = async() => {
     const query = `
-        SELECT sp.title AS title, c.name AS category
-      FROM public.service_projects sp
-      JOIN public.project_categories pc ON sp.project_id = pc.project_id
-      JOIN public.categories c ON pc.category_id = c.category_id
-      ORDER BY sp.project_id;
+      SELECT c.name AS category, ARRAY_AGG(sp.title) AS projects
+      FROM public.categories c
+      JOIN public.project_categories pc ON c.category_id = pc.category_id
+      JOIN public.service_projects sp ON sp.project_id = pc.project_id
+      GROUP BY c.name;
     `;
 
     const result = await db.query(query);
